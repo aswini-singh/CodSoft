e3c80da998077c28c6032564b9efef5f082476c2
aswini-singh <aswiniamarsingh@gmail.com>
2023-08-31 23:21:43 +0530

    useful changes

--- password_generator/password.py
+++ password_generator/password.py
@@ -52,20 +52,20 @@ x = (screen_width/2) - (window_width/2)
 y = (screen_height/2) - (window_height/2)
 root.geometry(f'{window_width}x{window_height}+{int(x)}+{int(y)}')
 
-length_label = tk.Label(root,text = "Enter Password Length",fg="blue",font=("Arial",15,"bold"))
+length_label = tk.Label(root,text = "Enter Password Length :",fg="blue",font=("Arial",15,"bold"))
 length_label.grid(row=0 , column=0,sticky="w")
 
 length_entry = tk.Entry(root)
 length_entry .grid(row =0,column=1,sticky="e")
 
-generate_butn = tk.Button(root,text="Generate password",command=generate_pw_butn,fg="red",bg="cyan")
+generate_butn = tk.Button(root,text="Generate password",command=generate_pw_butn,fg="red",font=("Arial",15,"bold"))
 generate_butn.grid(row=1,column=0,sticky="w")
 
 password_entry = tk.Entry(root,show ="")
 password_entry.grid(row=1,column=1,columnspan=2,sticky="w")
 
 show_butn = tk.Button(root,text='Show Password',command= password_visible ,state = "disabled",fg ='brown',
-              font = ("Arial",12,"bold"))
+              font = ("Arial",15,"bold"))
 show_butn.grid(row=2,column=0,columnspan=4)
 
 pw_visible = False
